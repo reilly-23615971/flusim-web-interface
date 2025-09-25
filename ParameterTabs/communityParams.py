Fixed toast background; halved slider lengths for better precision; lowered max value for disease life cycle sliders (and lowered precision for easier selection); stacked sliders in some multifields; made logs get hurled out of the application folder so that watchfiles doesn't complain

ParameterTabs/communityParams.py
@@ -91,7 +91,7 @@ def buildCommunityTab(id):
         loadKey(f'caseRatio', id, 0.5)
         st.select_slider(
             'Diagnosed Case Rate (Probability)', 
-            np.linspace(0.0, 1.0, 1001), 0.5, key = f'_caseRatio{id}', 
+            np.linspace(0.0, 1.0, 201), 0.5, key = f'_caseRatio{id}', 
             on_change = saveKey, args = [f'caseRatio', id], # type: ignore
             format_func = lambda x: f'{100 * x:0.3g}%', help = '''
                 The probability that an infected, symptomatic 
@@ -99,10 +99,10 @@ def buildCommunityTab(id):
                 confirmed case of the disease.
             '''
         )
-        loadKey(f'gpRatio', id, 0.333)
+        loadKey(f'gpRatio', id, 0.35)
         st.select_slider(
             'GP Visit Rate (Probability)', 
-            np.linspace(0.0, 1.0, 1001), 0.333, key = f'_gpRatio{id}', 
+            np.linspace(0.0, 1.0, 201), 0.35, key = f'_gpRatio{id}', 
             on_change = saveKey, args = [f'gpRatio', id], # type: ignore
             format_func = lambda x: f'{100 * x:0.3g}%', help = '''
                 The probability that an infected, symptomatic 
@@ -113,7 +113,7 @@ def buildCommunityTab(id):
         loadKey(f'hospitalRatio', id, 0.25)
         st.select_slider(
             'Hospitalisation Rate (Probability)', 
-            np.linspace(0.0, 1.0, 1001), 0.25, key = f'_hospitalRatio{id}',
+            np.linspace(0.0, 1.0, 201), 0.25, key = f'_hospitalRatio{id}',
             on_change = saveKey, args = [f'hospitalRatio', id], # type: ignore
             format_func = lambda x: f'{100 * x:0.3g}%', help = '''
                 The probability that an infected, symptomatic 
@@ -124,7 +124,7 @@ def buildCommunityTab(id):
         loadKey(f'icuRatio', id, 0.1)
         st.select_slider(
             'ICU Visit Rate (Probability)', 
-            np.linspace(0.0, 1.0, 1001), 0.1, key = f'_icuRatio{id}', 
+            np.linspace(0.0, 1.0, 201), 0.1, key = f'_icuRatio{id}', 
             on_change = saveKey, args = [f'icuRatio', id], # type: ignore
             format_func = lambda x: f'{100 * x:0.3g}%', help = '''
                 The probability that an infected, symptomatic 
@@ -136,7 +136,7 @@ def buildCommunityTab(id):
         loadKey(f'deathRatio', id, 0.05)
         deathRate = st.select_slider(
             'Mortality Rate (Probability)', 
-            np.linspace(0.0, 1.0, 1001), 0.05, key = f'_deathRatio{id}', 
+            np.linspace(0.0, 1.0, 201), 0.05, key = f'_deathRatio{id}', 
             on_change = saveKey, args = [f'deathRatio', id], # type: ignore
             format_func = lambda x: f'{100 * x:0.3g}%', help = '''
                 The base probability that an infected, symptomatic 
@@ -162,7 +162,9 @@ def buildCommunityTab(id):
         for i in range(deathRowCount): 
             (
                 deathGroupColumn, deathRateColumn, deathRemoveColumn
-            ) = deathAgeContainer.columns((0.25, 0.55, 0.2))
+            ) = deathAgeContainer.columns(
+                (0.25, 0.55, 0.2), vertical_alignment = 'center'
+            )
             deathCurrentGroup = st.session_state.get(
                 f'deathAgeGroup{id}-{i}'
             )
@@ -206,7 +208,7 @@ def buildCommunityTab(id):
             loadKey(f'deathRatio', id, 0.05, f'-{i}')
             with deathRateColumn: st.select_slider(
                 'Mortality Rate (Probability)', 
-                np.linspace(0.0, 1.0, 1001), 0.05, 
+                np.linspace(0.0, 1.0, 201), 0.05, 
                 key = f'_deathRatio{id}-{i}', 
                 on_change = saveKey, args = [f'deathRatio', id, f'-{i}'], # type: ignore
                 format_func = lambda x: f'{100 * x:0.3g}%', help = '''
@@ -275,7 +277,7 @@ def buildCommunityTab(id):
         loadKey(f'withdrawalWork', id, 0.5)
         st.select_slider(
             'Work Withdrawal Rate (Probability)', 
-            np.linspace(0.0, 1.0, 1001), 0.5, 
+            np.linspace(0.0, 1.0, 201), 0.5, 
             format_func = lambda x: f'{100 * x:0.3g}%',
             on_change = saveKey, args = [f'withdrawalWork', id], # type: ignore
             key = f'_withdrawalWork{id}', help = '''
@@ -287,7 +289,7 @@ def buildCommunityTab(id):
         loadKey(f'withdrawalSchool', id, 0.9)
         st.select_slider(
             'School Withdrawal Rate (Probability)', 
-            np.linspace(0.0, 1.0, 1001), 0.9, 
+            np.linspace(0.0, 1.0, 201), 0.9, 
             format_func = lambda x: f'{100 * x:0.3g}%', 
             on_change = saveKey, args = [f'withdrawalSchool', id], # type: ignore
             key = f'_withdrawalSchool{id}', help = '''
@@ -340,7 +342,7 @@ def buildCommunityTab(id):
         loadKey(f'childSupervision', id, 1.0)
         st.select_slider(
             'Child Supervision Rate (Probability)',
-            np.linspace(0.0, 1.0, 1001), 1.0, 
+            np.linspace(0.0, 1.0, 201), 1.0, 
             key = f'_childSupervision{id}', 
             on_change = saveKey, args = [f'childSupervision', id], # type: ignore
             format_func = lambda x: f'{100 * x:0.3g}%', help = '''

ParameterTabs/diseaseParams.py
@@ -100,7 +100,7 @@ def buildDiseaseTab(id):
         loadKey(f'seedRate', id, 0.25)
         st.select_slider(
             'Infection Seeding Rate (Average Individuals per Day)', 
-            np.linspace(0.005, 5.0, 1000), 0.25, key = f'_seedRate{id}', 
+            np.linspace(0.025, 5.0, 200), 0.25, key = f'_seedRate{id}', 
             on_change = saveKey, args = [f'seedRate', id], # type: ignore
             format_func = lambda x: f'{x:0.4g}', help = '''
                 The average number of individuals that will be 
@@ -265,7 +265,7 @@ def buildDiseaseTab(id):
         loadKey(f'beta', id, 0.11)
         st.select_slider(
             'Basic Transmission Parameter (β)', 
-            np.linspace(0.001, 1.0, 1000), 0.11, 
+            np.linspace(0.005, 1.0, 200), 0.11, 
             format_func = lambda x: f'{x:0.3g}', key = f'_beta{id}', 
             on_change = saveKey, args = [f'beta', id], # type: ignore
             help = '''
@@ -282,7 +282,7 @@ def buildDiseaseTab(id):
         loadKey(f'betaAsymptomatic', id, 0.55)
         st.select_slider(
             'Asymptomatic Transmission Multiplier', 
-            np.linspace(0.0, 1.0, 1001), 0.55, 
+            np.linspace(0.0, 1.0, 201), 0.55, 
             format_func = lambda x: f'{x:0.3g}', 
             on_change = saveKey, args = [f'betaAsymptomatic', id], # type: ignore
             key = f'_betaAsymptomatic{id}', help = '''
@@ -302,7 +302,7 @@ def buildDiseaseTab(id):
         loadKey(f'betaPostSymptomatic', id, 0.55)
         st.select_slider(
             'Post-Symptomatic Transmission Multiplier', 
-            np.linspace(0.0, 1.0, 1001), 0.55, 
+            np.linspace(0.0, 1.0, 201), 0.55, 
             format_func = lambda x: f'{x:0.3g}', 
             on_change = saveKey, args = [f'betaPostSymptomatic', id], # type: ignore
             key = f'_betaPostSymptomatic{id}', help = '''
@@ -319,7 +319,7 @@ def buildDiseaseTab(id):
         loadKey(f'householdKappa', id, 2.2)
         st.select_slider(
             'Household Transmission Multiplier', 
-            np.linspace(0.0, 5.0, 1001), 2.2, key = f'_householdKappa{id}',
+            np.linspace(0.0, 5.0, 201), 2.2, key = f'_householdKappa{id}',
             on_change = saveKey, args = [f'householdKappa', id], # type: ignore
             format_func = lambda x: f'{x:0.4g}', help = '''
                 The value of the transmissibility modifier 
@@ -351,9 +351,11 @@ def buildDiseaseTab(id):
         transAgeContainer = st.container()
         for i in range(transRowCount): 
             (
-                transGroupColumn, transInfectColumn, 
-                transSusceptColumn, transRemoveColumn
-            ) = transAgeContainer.columns((0.25, 0.275, 0.275, 0.2))
+                transGroupColumn, transInfectColumn, #transSusceptColumn, 
+                transRemoveColumn
+            ) = transAgeContainer.columns(
+                (0.25, 0.55, 0.2), vertical_alignment = 'center'
+            )
             transCurrentGroup = st.session_state.get(
                 f'transAgeGroup{id}-{i}'
             )
@@ -397,7 +399,7 @@ def buildDiseaseTab(id):
             # Infectiousness column
             loadKey(f'transInfect', id, 1.0, f'-{i}')
             with transInfectColumn: st.select_slider(
-                'Infectiousness', np.linspace(0.0, 1.0, 1001), 1.0, 
+                'Infectiousness', np.linspace(0.0, 3.0, 301), 1.0, 
                 key = f'_transInfect{id}-{i}', 
                 on_change = saveKey, args = [f'transInfect', id, f'-{i}'], # type: ignore
                 format_func = lambda x: f'{x:0.3g}', help = '''
@@ -412,8 +414,8 @@ def buildDiseaseTab(id):
             )
             # Susceptibility column
             loadKey(f'transSuscept', id, 1.0, f'-{i}')
-            with transSusceptColumn: st.select_slider(
-                'Susceptibility', np.linspace(0.0, 1.0, 1001), 1.0, 
+            with transInfectColumn: st.select_slider(
+                'Susceptibility', np.linspace(0.0, 3.0, 301), 1.0, 
                 key = f'_transSuscept{id}-{i}', 
                 on_change = saveKey, args = [f'transSuscept', id, f'-{i}'], # type: ignore
                 format_func = lambda x: f'{x:0.3g}', help = '''
@@ -490,7 +492,9 @@ def buildDiseaseTab(id):
             (
                 kappaLocationColumn, kappaValueColumn, 
                 kappaRemoveColumn
-            ) = kappaContainer.columns((0.25, 0.55, 0.2))
+            ) = kappaContainer.columns(
+                (0.25, 0.55, 0.2), vertical_alignment = 'center'
+            )
             kappaCurrentLocation = st.session_state.get(
                 f'kappaLocation{id}-{i}'
             )
@@ -543,7 +547,7 @@ def buildDiseaseTab(id):
             # Kappa value column
             loadKey(f'kappaValue', id, 1.0, f'-{i}')
             with kappaValueColumn: st.select_slider(
-                'Transmission Multiplier', np.linspace(0.0, 1.0, 1001), 
+                'Transmission Multiplier', np.linspace(0.0, 1.0, 201), 
                 1.0, key = f'_kappaValue{id}-{i}', 
                 on_change = saveKey, args = [f'kappaValue', id, f'-{i}'], # type: ignore
                 format_func = lambda x: f'{x:0.3g}', help = '''
@@ -609,7 +613,7 @@ def buildDiseaseTab(id):
         loadKey(f'asymptomaticChild', id, 0.35)
         st.select_slider(
             'Probability of Young (0-24) Asymptomatic Case', 
-            np.linspace(0.0, 1.0, 1001), 0.35, 
+            np.linspace(0.0, 1.0, 201), 0.35, 
             format_func = lambda x: f'{100 * x:0.3g}%', 
             on_change = saveKey, args = [f'asymptomaticChild', id], # type: ignore
             key = f'_asymptomaticChild{id}', help = '''
@@ -622,7 +626,7 @@ def buildDiseaseTab(id):
         loadKey(f'asymptomaticAdult', id, 0.35)
         st.select_slider(
             'Probability of Adult (24+) Asymptomatic Case', 
-            np.linspace(0.0, 1.0, 1001), 0.35, 
+            np.linspace(0.0, 1.0, 201), 0.35, 
             format_func = lambda x: f'{100 * x:0.3g}%', 
             on_change = saveKey, args = [f'asymptomaticAdult', id], # type: ignore
             key = f'_asymptomaticAdult{id}', help = '''
@@ -666,7 +670,7 @@ def buildDiseaseTab(id):
         # Alternate period definitions
         loadKey(f'latencyPeriod', id, 10)
         latencyPeriod = st.select_slider(
-            'Latency Period Length (Days)', range(51), 10, 
+            'Latency Period Length (Days)', range(22), 10, 
             on_change = saveKey, args = [f'latencyPeriod', id], # type: ignore
             format_func = dayCount, key = f'_latencyPeriod{id}', help = '''
                 The length in days of the disease's latency period, 
@@ -677,7 +681,7 @@ def buildDiseaseTab(id):
         )
         loadKey(f'preSymptomPeriod', id, 2)
         preSymptomPeriod = st.select_slider(
-            'Pre-Symptomatic Period Length (Days)', range(51), 2, 
+            'Pre-Symptomatic Period Length (Days)', range(22), 2, 
             format_func = dayCount, key = f'_preSymptomPeriod{id}', 
             on_change = saveKey, args = [f'preSymptomPeriod', id], # type: ignore
             help = '''
@@ -690,7 +694,7 @@ def buildDiseaseTab(id):
         )
         loadKey(f'symptomPeriod', id, 7)
         symptomPeriod = st.select_slider(
-            'Symptomatic Period Length (Days)', range(51), 7, 
+            'Symptomatic Period Length (Days)', range(22), 7, 
             on_change = saveKey, args = [f'symptomPeriod', id], # type: ignore
             format_func = dayCount, key = f'_symptomPeriod{id}', help = '''
                 The length in days of the disease's symptomatic 
@@ -701,7 +705,7 @@ def buildDiseaseTab(id):
         )
         loadKey(f'postSymptomPeriod', id, 1)
         postSymptomPeriod = st.select_slider(
-            'Post-Symptomatic Period Length (Days)', range(51), 1, 
+            'Post-Symptomatic Period Length (Days)', range(22), 1, 
             format_func = dayCount, key = f'_postSymptomPeriod{id}', 
             on_change = saveKey, args = [f'postSymptomPeriod', id], # type: ignore
             help = '''
@@ -751,7 +755,7 @@ def buildDiseaseTab(id):
             periods can be defined:
         ''')
         totalCol, incubationCol, infectiousCol = st.columns(
-            (0.33333, 0.33333, 0.33333)
+            (0.33333, 0.33333, 0.33333), vertical_alignment = 'center'
         )
         totalCol.metric(
             'Total Length of Infection', dayCount(
@@ -820,7 +824,7 @@ def buildDiseaseTab(id):
         loadKey(f'naturalWanedEfficacy', id, 0.5)
         st.select_slider(
             'Natural Immunity After Waning (Probability)',
-            np.linspace(0.0, 1.0, 1001), 0.5, 
+            np.linspace(0.0, 1.0, 201), 0.5, 
             key = f'_naturalWanedEfficacy{id}', 
             on_change = saveKey, args = [f'naturalWanedEfficacy', id], # type: ignore
             format_func = lambda x: f'{100 * x:0.3g}%', help = '''

ParameterTabs/dynamicParams.py
@@ -99,15 +99,17 @@ def buildDynamicTab(id):
     for i in range(seedRowCount): 
         (
             seedCycleColumn, seedNewColumn, seedRemoveColumn
-        ) = seedContainer.columns((0.4, 0.4, 0.2))
+        ) = seedContainer.columns(
+            (0.4, 0.4, 0.2), vertical_alignment = 'center'
+        )
         # Cycle column
         loadKey(f'seedCycle', id, 15, f'-{i}')
         with seedCycleColumn: 
-            seedUpdatePoint = st.select_slider(
-                'Day to Update Parameter', range(720), 
+            seedUpdatePoint = st.number_input(
+                'Day to Update Parameter', 1, 720, 
                 15, key = f'_seedCycle{id}-{i}', 
                 on_change = saveKey, args = [f'seedCycle', id, f'-{i}'], # type: ignore
-                format_func = lambda x: f'Day {x + 1}', help = '''
+                placeholder = 'Enter the day number', help = '''
                     The day of the simulation upon which the new 
                     value for infection seeding rate will come into 
                     effect.
@@ -241,7 +243,7 @@ def buildDynamicTab(id):
         loadKey(f'seedNewRate', id, 0.25, f'-{i}')
         with seedNewColumn: st.select_slider(
             'New Value (Average Individuals per Day)', 
-            np.linspace(0.005, 5.0, 1000), 0.25, 
+            np.linspace(0.025, 5.0, 200), 0.25, 
             key = f'_seedNewRate{id}-{i}', 
             on_change = saveKey, args = [f'seedNewRate', id, f'-{i}'], # type: ignore
             format_func = lambda x: f'{x:0.4g}', help = '''
@@ -319,12 +321,14 @@ def buildDynamicTab(id):
     for i in range(closeRowCount): 
         (
             closeCycleColumn, closeNewColumn, closeRemoveColumn
-        ) = closeContainer.columns((0.4, 0.4, 0.2))
+        ) = closeContainer.columns(
+            (0.4, 0.4, 0.2), vertical_alignment = 'center'
+        )
         # Cycle column
         loadKey(f'closeCycle', id, 15, f'-{i}')
         with closeCycleColumn: 
-            closeUpdatePoint = st.select_slider(
-                'Day to Update Parameter', range(720), 15, 
+            closeUpdatePoint = st.number_input(
+                'Day to Update Parameter', 0, 720, 15, 
                 key = f'_closeCycle{id}-{i}', disabled = not closeActive, 
                 on_change = saveKey, args = [f'closeCycle', id, f'-{i}'], # type: ignore
                 format_func = lambda x: f'Day {x + 1}', help = '''
@@ -476,7 +480,7 @@ def buildDynamicTab(id):
         loadKey(f'closeNewRate', id, 0.9, f'-{i}')
         with closeNewColumn: st.select_slider(
             'New Value (Probability)', 
-            np.linspace(0.0, 1.0, 1001), 0.9, 
+            np.linspace(0.0, 1.0, 201), 0.9, 
             key = f'_closeNewRate{id}-{i}', disabled = not closeActive, 
             on_change = saveKey, args = [f'closeNewRate', id, f'-{i}'], # type: ignore
             format_func = lambda x: f'{100 * x:0.3g}%', help = '''
@@ -557,12 +561,14 @@ def buildDynamicTab(id):
     for i in range(bccRowCount): 
         (
             bccCycleColumn, bccNewColumn, bccRemoveColumn
-        ) = bccContainer.columns((0.4, 0.4, 0.2))
+        ) = bccContainer.columns(
+            (0.4, 0.4, 0.2), vertical_alignment = 'center'
+        )
         # Cycle column
         loadKey(f'bccCycle', id, 15, f'-{i}')
         with bccCycleColumn: 
-            bccUpdatePoint = st.select_slider(
-                'Day to Update Parameter', range(720), 
+            bccUpdatePoint = st.number_input(
+                'Day to Update Parameter', 0, 720, 
                 15, key = f'_bccCycle{id}-{i}', disabled = not bccActive, 
                 on_change = saveKey, args = [f'bccCycle', id, f'-{i}'], # type: ignore
                 format_func = lambda x: f'Day {x + 1}', help = '''

ParameterTabs/vaccinationNPIParams.py
@@ -421,7 +421,7 @@ def buildVaccinationNPITab(id):
             loadKey(f'initialVaccinated', id, 0.0)
             initialVaccinated = st.select_slider(
                 'Initial Vaccinated Proportion of Population', 
-                np.linspace(0.0, 1.0, 1001), 0.0, 
+                np.linspace(0.0, 1.0, 201), 0.0, 
                 key = f'_initialVaccinated{id}', 
                 format_func = lambda x: f'{100 * x:0.3g}%', 
                 on_change = saveKey, args = [f'initialVaccinated', id], # type: ignore
@@ -434,7 +434,7 @@ def buildVaccinationNPITab(id):
             loadKey(f'targetVaccinated', id, 0.8)
             targetVaccinated = st.select_slider(
                 'Target Vaccinated Proportion of Population', 
-                np.linspace(0.0, 1.0, 1001), 0.8, 
+                np.linspace(0.0, 1.0, 201), 0.8, 
                 key = f'_targetVaccinated{id}', 
                 format_func = lambda x: f'{100 * x:0.3g}%', 
                 on_change = saveKey, args = [f'targetVaccinated', id], # type: ignore
@@ -532,10 +532,9 @@ def buildVaccinationNPITab(id):
             vacAgeProportionContainer = st.container()
             for i in range(vaccineRowCount): 
                 (
-                    vacAgeGroupColumn, vacAgeInitialColumn, 
-                    vacAgeTargetColumn, vacAgeRemoveColumn
+                    vacAgeGroupColumn, vacAgeInitialColumn, vacAgeRemoveColumn
                 ) = vacAgeProportionContainer.columns(
-                    (0.25, 0.275, 0.275, 0.2)
+                    (0.25, 0.55, 0.2), vertical_alignment = 'center'
                 )
                 vacAgeCurrentGroup = st.session_state.get(
                     f'vacAgeGroup{id}-{i}'
@@ -585,7 +584,7 @@ def buildVaccinationNPITab(id):
                 with vacAgeInitialColumn: 
                     vacAgeInitials[vacAgeGroup] = st.select_slider(
                         'Initial Vaccinated Proportion of Population', 
-                        np.linspace(0.0, 1.0, 1001), 0.0,  
+                        np.linspace(0.0, 1.0, 201), 0.0,  
                         format_func = lambda x: f'{100 * x:0.3g}%',
                         disabled = not useVaccinesToggle, 
                         on_change = saveKey, 
@@ -599,10 +598,10 @@ def buildVaccinationNPITab(id):
                     )
                 # Target proportion column
                 loadKey(f'vacAgeTarget', id, 0.8, f'-{i}')
-                with vacAgeTargetColumn: 
+                with vacAgeInitialColumn: 
                     vacAgeTargets[vacAgeGroup] = st.select_slider(
                         'Target Vaccinated Proportion of Population', 
-                        np.linspace(0.0, 1.0, 1001), 0.8, 
+                        np.linspace(0.0, 1.0, 201), 0.8, 
                         format_func = lambda x: f'{100 * x:0.3g}%',
                         disabled = not useVaccinesToggle, 
                         on_change = saveKey, 
@@ -806,7 +805,7 @@ def buildVaccinationNPITab(id):
             loadKey(f'primaryWanedEfficacy', id, 0.0)
             primaryWanedEfficacy = st.select_slider(
                 'Dose Efficacy After Immunity Waning (Probability)', 
-                np.linspace(0.0, 1.0, 1001), 0.0, 
+                np.linspace(0.0, 1.0, 201), 0.0, 
                 format_func = lambda x: f'{100 * x:0.3g}%', 
                 disabled = not useVaccinesToggle, 
                 on_change = saveKey, args = [f'primaryWanedEfficacy', id], # type: ignore
@@ -864,7 +863,9 @@ def buildVaccinationNPITab(id):
                 (
                     primWanedGroupColumn, primWanedEffColumn, 
                     primWanedRemoveColumn
-                ) = primWanedContainer.columns((0.25, 0.55, 0.2))
+                ) = primWanedContainer.columns(
+                    (0.25, 0.55, 0.2), vertical_alignment = 'center'
+                )
                 primWanedCurrentGroup = st.session_state.get(
                     f'primWanedGroup{id}-{i}'
                 )
@@ -919,7 +920,7 @@ def buildVaccinationNPITab(id):
                             'Dose Efficacy After Immunity '
                             'Waning (Probability)'
                         )), 
-                        np.linspace(0.0, 1.0, 1001), 0.0, 
+                        np.linspace(0.0, 1.0, 201), 0.0, 
                         format_func = lambda x: f'{100 * x:0.3g}%', 
                         disabled = not useVaccinesToggle, 
                         on_change = saveKey, 
@@ -1000,7 +1001,7 @@ def buildVaccinationNPITab(id):
                 loadKey(f'primaryBaseEfficacy', id, 0.5, f'-{i}')
                 baseDoseEfficacy = doseEfficacyContainer.select_slider(
                     'Initial Dose Efficacy (Probability)', 
-                    np.linspace(0.0, 1.0, 1001), 0.5, 
+                    np.linspace(0.0, 1.0, 201), 0.5, 
                     format_func = lambda x: f'{100 * x:0.3g}%', 
                     disabled = not useVaccinesToggle, on_change = saveKey, 
                     args = [f'primaryBaseEfficacy', id, f'-{i}'], # type: ignore
@@ -1112,7 +1113,7 @@ def buildVaccinationNPITab(id):
                         primAgeGroupColumn, primAgeEfficacyColumn, 
                         primAgeRemoveColumn
                     ) = primEfficacyContainer.columns(
-                        (0.25, 0.55, 0.2)
+                        (0.25, 0.55, 0.2), vertical_alignment = 'center'
                     )
                     primAgeCurrentGroup = st.session_state.get(
                         f'primAgeGroup{id}-{i}-{j}'
@@ -1165,7 +1166,7 @@ def buildVaccinationNPITab(id):
                     with primAgeEfficacyColumn: 
                         ageInitialEfficacy = st.select_slider(
                             'Initial Dose Efficacy (Probability)',
-                            np.linspace(0.0, 1.0, 1001), 0.5, 
+                            np.linspace(0.0, 1.0, 201), 0.5, 
                             format_func = lambda x: f'{100 * x:0.3g}%', 
                             disabled = not useVaccinesToggle, 
                             key = f'_primAgeEfficacy{id}-{i}-{j}', 
@@ -1451,7 +1452,7 @@ def buildVaccinationNPITab(id):
             loadKey(f'boosterBaseEfficacy', id, 0.9)
             boosterBaseEfficacy = st.select_slider(
                 'Initial Booster Efficacy (Probability)', 
-                np.linspace(0.0, 1.0, 1001), 0.9, 
+                np.linspace(0.0, 1.0, 201), 0.9, 
                 key = f'_boosterBaseEfficacy{id}',
                 disabled = not useVaccinesToggle or not useBoostersToggle, 
                 on_change = saveKey, args = [f'boosterBaseEfficacy', id], # type: ignore
@@ -1466,7 +1467,7 @@ def buildVaccinationNPITab(id):
             loadKey(f'boosterWanedEfficacy', id, 0.6)
             boosterWanedEfficacy = st.select_slider(
                 'Booster Efficacy After Immunity Waning (Probability)',
-                np.linspace(0.0, 1.0, 1001), 0.6, 
+                np.linspace(0.0, 1.0, 201), 0.6, 
                 key = f'_boosterWanedEfficacy{id}', 
                 disabled = not useVaccinesToggle or not useBoostersToggle,
                 on_change = saveKey, args = [f'boosterWanedEfficacy', id], # type: ignore
@@ -1587,9 +1588,9 @@ def buildVaccinationNPITab(id):
             for i in range(boosterRowCount):
                 (
                     boostAgeGroupColumn, boostAgeEfficacyColumn, 
-                    boostAgeWanedColumn, boostAgeRemoveColumn
+                    boostAgeRemoveColumn
                 ) = boostAgeEfficacyContainer.columns(
-                    (0.25, 0.275, 0.275, 0.2)
+                    (0.25, 0.55, 0.2), vertical_alignment = 'center'
                 )
                 boostAgeCurrentGroup = st.session_state.get(
                     f'boostAgeGroup{id}-{i}'
@@ -1641,7 +1642,7 @@ def buildVaccinationNPITab(id):
                 with boostAgeEfficacyColumn: 
                     boostAgeInitials[boostAgeGroup] = st.select_slider(
                         'Initial Booster Efficacy (Probability)', 
-                        np.linspace(0.0, 1.0, 1001), 0.9, disabled = (
+                        np.linspace(0.0, 1.0, 201), 0.9, disabled = (
                             not useVaccinesToggle or not useBoostersToggle
                         ), 
                         format_func = lambda x: f'{100 * x:0.3g}%', 
@@ -1658,13 +1659,13 @@ def buildVaccinationNPITab(id):
                     )
                 # Waned efficacy column
                 loadKey(f'boostAgeWanedEfficacy', id, 0.6, f'-{i}')
-                with boostAgeWanedColumn: 
+                with boostAgeEfficacyColumn: 
                     boostAgeWaneds[boostAgeGroup] = st.select_slider(
                         ((
                             'Booster Efficacy After '
                             'Immunity Waning (Probability)'
                         )), 
-                        np.linspace(0.0, 1.0, 1001), 0.6, disabled = (
+                        np.linspace(0.0, 1.0, 201), 0.6, disabled = (
                             not useVaccinesToggle or not useBoostersToggle
                         ), 
                         format_func = lambda x: f'{100 * x:0.3g}%', 
@@ -1853,7 +1854,7 @@ def buildVaccinationNPITab(id):
             loadKey(f'socialDistancingCompliance', id, 0.9)
             socialDistancingCompliance = st.select_slider(
                 'Social Distancing Compliance (Probability)', 
-                np.linspace(0.0, 1.0, 1001), 0.9, 
+                np.linspace(0.0, 1.0, 201), 0.9, 
                 format_func = lambda x: f'{100 * x:0.3g}%', 
                 disabled = not useSocialDistancingToggle, 
                 on_change = saveKey, 
@@ -1883,7 +1884,9 @@ def buildVaccinationNPITab(id):
                 (
                     socialGroupColumn, socialComplianceColumn, 
                     socialRemoveColumn
-                ) = socialAgeContainer.columns((0.25, 0.55, 0.2))
+                ) = socialAgeContainer.columns(
+                    (0.25, 0.55, 0.2), vertical_alignment = 'center'
+                )
                 socialCurrentGroup = st.session_state.get(
                     f'socialAgeGroup{id}-{i}'
                 )
@@ -1933,7 +1936,7 @@ def buildVaccinationNPITab(id):
                 loadKey(f'socialCompliance', id, 0.9, f'-{i}')
                 with socialComplianceColumn: st.select_slider(
                     'Social Distancing Compliance (Probability)', 
-                    np.linspace(0.0, 1.0, 1001), 0.9, 
+                    np.linspace(0.0, 1.0, 201), 0.9, 
                     format_func = lambda x: f'{100 * x:0.3g}%', 
                     disabled = not useSocialDistancingToggle, 
                     on_change = saveKey, 
@@ -2311,7 +2314,7 @@ def buildVaccinationNPITab(id):
             loadKey(f'schoolClosureCompliance', id, 0.9)
             st.select_slider(
                 'School Closure Compliance (Probability)', 
-                np.linspace(0.0, 1.0, 1001), 0.9, 
+                np.linspace(0.0, 1.0, 201), 0.9, 
                 format_func = lambda x: f'{100 * x:0.3g}%',
                 disabled = not useSchoolClosureToggle, 
                 on_change = saveKey, 
@@ -2622,7 +2625,7 @@ def buildVaccinationNPITab(id):
             loadKey(f'withdrawalIncreaseAdult', id, 0.9)
             withdrawalIncreaseAdult = st.select_slider(
                 'Adult Increased Withdrawal Rate (Probability)', 
-                np.linspace(0.0, 1.0, 1001), 0.9, 
+                np.linspace(0.0, 1.0, 201), 0.9, 
                 format_func = lambda x: f'{100 * x:0.3g}%',
                 disabled = not useWithdrawalIncreaseToggle, 
                 on_change = saveKey, 
@@ -2639,7 +2642,7 @@ def buildVaccinationNPITab(id):
             loadKey(f'withdrawalIncreaseChild', id, 1.0)
             withdrawalIncreaseChild = st.select_slider(
                 'Child Increased Withdrawal Rate (Probability)', 
-                np.linspace(0.0, 1.0, 1001), 1.0, 
+                np.linspace(0.0, 1.0, 201), 1.0, 
                 format_func = lambda x: f'{100 * x:0.3g}%', 
                 disabled = not useWithdrawalIncreaseToggle, 
                 on_change = saveKey, 