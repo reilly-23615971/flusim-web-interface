1f518768c0f92e802da62ce6929d6c4a55cf1b5d
reilly-23615971 <23615971@student.uwa.edu.au>
2025-09-26 15:55:46 +0800

    Scenario names are now obtained correctly for preset data

--- ClientResources/SimulationRunFunctions.py
+++ ClientResources/SimulationRunFunctions.py
@@ -159,11 +159,59 @@ simulation.
             st.session_state.simulationInProgress = True
             st.session_state.simulationStartTime = datetime.now()
 
-            # Get scenario names
-            scenarioNames = ['Baseline'] + [
-                st.session_state[f'scenarioName{i}'] 
-                for i in range(1, scenarioCount + 1)
-            ]
+            # Create the final model JSON
+            # For testing use this JSON instead of parameters
+            if usePresetParams: 
+                parameterJSON = {
+                    "name": "Simple Test",
+                    "description": "2184",
+                    "output_folder": "./results/",
+                    "middle_joint": "-usingEpidemic_Emean",
+                    "community_used": ["newcastle"],
+                    "shared_overrides": {
+                        "parameters": {
+                            "Command_Argument": {"n_runs": 16,"n_cycles": 180},
+                            "Scenario_Strain": [{"StrainId": 0,"Beta": 0.11}],
+                            "Scenario_Parameter": {
+                                "school_closure_trigger": "timed",
+                                "school_closure_compliance": 0.5,
+                                "school_closure_delay": 28,
+                                "withdrawal_increase_trigger": "timed",
+                                "withdrawal_increase_delay": 28,
+                                "work_nonattendance_trigger": "timed",
+                                "prob_work_nonattendance": 0.5,
+                                "work_nonattendance_delay": 28
+                            }
+                        }
+                    },
+                    "override_templates": [{
+                        "name": "test_1",
+                        "parameters": {
+                            "Scenario_Parameter": {
+                                "seed_rate": 1.5
+                            }
+                        }
+                    }],
+                    "simulation_sets": [{
+                        "name": "test_set_1",
+                        "version": 2184,
+                        "simulations": [
+                            {"name": "test_sim_1"},
+                            {"name": "test_sim_2", "apply_template": ["test_1"]}
+                        ]
+                    }]
+                }
+                scenarioNames = ['Base Rate', 'Boosted Rate']
+
+            # Use this version in production
+            else: 
+                parameterJSON = createConfig(scenarioCount + 1).model_dump_json(
+                    indent = 4, exclude_unset = True#, exclude_defaults = True
+                )
+                scenarioNames = ['Baseline'] + [
+                    st.session_state[f'scenarioName{i}'] 
+                    for i in range(1, scenarioCount + 1)
+                ]
 
             # Save current parameter values that'll be used for 
             # visualisation when the user has potentially changed them
@@ -197,53 +245,6 @@ simulation.
                     st.session_state.PendingDataScenarioCount + 1
                 )
             }
-
-            # Create the final model JSON
-            # For testing use this JSON instead of parameters
-            if usePresetParams: parameterJSON = {
-                "name": "Simple Test",
-                "description": "2184",
-                "output_folder": "./results/",
-                "middle_joint": "-usingEpidemic_Emean",
-                "community_used": ["newcastle"],
-                "shared_overrides": {
-                    "parameters": {
-                        "Command_Argument": {"n_runs": 16,"n_cycles": 180},
-                        "Scenario_Strain": [{"StrainId": 0,"Beta": 0.11}],
-                        "Scenario_Parameter": {
-                            "school_closure_trigger": "timed",
-                            "school_closure_compliance": 0.5,
-                            "school_closure_delay": 28,
-                            "withdrawal_increase_trigger": "timed",
-                            "withdrawal_increase_delay": 28,
-                            "work_nonattendance_trigger": "timed",
-                            "prob_work_nonattendance": 0.5,
-                            "work_nonattendance_delay": 28
-                        }
-                    }
-                },
-                "override_templates": [{
-                    "name": "test_1",
-                    "parameters": {
-                        "Scenario_Parameter": {
-                            "seed_rate": 1.5
-                        }
-                    }
-                }],
-                "simulation_sets": [{
-                    "name": "test_set_1",
-                    "version": 2184,
-                    "simulations": [
-                        {"name": "test_sim_1"},
-                        {"name": "test_sim_2", "apply_template": ["test_1"]}
-                    ]
-                }]
-            }
-
-            # Use this version in production
-            else: parameterJSON = createConfig(scenarioCount + 1).model_dump_json(
-                indent = 4, exclude_unset = True#, exclude_defaults = True
-            )
 
             # Make the model call
             runModelWrapper(scenarioNames, parameterJSON)
