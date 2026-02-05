e2adc95064c37cab5f3cc7f1cedc725e5f37a7b4
reilly-23615971 <23615971@student.uwa.edu.au>
2026-02-05 09:37:04 +0800

    Regularised dashboardApp.py's formatting

--- dashboardApp.py
+++ dashboardApp.py
@@ -3,8 +3,9 @@
 # Main page of dashboard, defining pages & setting universal parameters
 
 # Imports
-import os, logging
-from datetime import datetime, timedelta
+import os
+import logging
+from datetime import datetime
 import pandas as pd
 import streamlit as st
 import streamlit_notify as stn
@@ -14,28 +15,29 @@ from ClientResources.SimulationRunFunctions import runSimulationButton
 # Set this early to minimise the time spent with a different page title
 # TODO: Populate About section with credits to SMRG-Flusim and Paul Tol
 st.set_page_config(
-    page_title = 'SMRG Flusim Web Dashboard', 
-    page_icon = ':material/microbiology:',
-    menu_items = {'About': '''
+    page_title="SMRG Flusim Web Dashboard",
+    page_icon=":material/microbiology:",
+    menu_items={
+        "About": """
         ## SMRG Flusim Web Dashboard
-                  
-        This dashboard is designed to work with the *Flusim* model 
-        designed by the UWA Software Modelling Research Group. 
-                  
+        This dashboard is designed to work with the *Flusim* model
+        designed by the UWA Software Modelling Research Group.
         ##### Additional Credits
-                  
         Colour palette for scenarios created by Paul Tol
         https://sronpersonalpages.nl/~pault/
-    '''}
+    """
+    },
 )
 
-# Logging config (create log folder outside of project dir to avoid 
+# Logging config (create log folder outside of project dir to avoid
 # watchfiles getting into an endless update loop)
-os.makedirs('../Logs', exist_ok = True)
+os.makedirs("../Logs", exist_ok=True)
 logging.basicConfig(
-    filename = '../Logs/interfaceAppLogs.txt', filemode = 'a', 
-    format = '%(asctime)s,%(msecs)03d %(name)s %(levelname)s %(message)s', 
-    datefmt = '%Y-%m-%d %H:%M:%S', level = logging.INFO
+    filename="../Logs/interfaceAppLogs.txt",
+    filemode="a",
+    format="%(asctime)s,%(msecs)03d %(name)s %(levelname)s %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    level=logging.INFO,
 )
 
 appLog = logging.getLogger(__name__)
@@ -44,53 +46,57 @@ appLog = logging.getLogger(__name__)
 session = st.session_state
 
 # Set environment variables for config
-#os.environ['STREAMLIT_GLOBAL_DISABLE_WIDGET_STATE_DUPLICATION_WARNING'] = '1'
-
+# os.environ['STREAMLIT_GLOBAL_DISABLE_WIDGET_STATE_DUPLICATION_WARNING'] = '1'
 
 
 # Define application pages
-# TODO: Confirm ideal page layout/what goes where
 modelDescription = st.Page(
-    'DashboardPages/modelDescription.py', title = 'Model Description', 
-    icon = ':material/description:'
+    "DashboardPages/modelDescription.py",
+    title="Model Description",
+    icon=":material/description:",
 )
 baselineParameters = st.Page(
-    'DashboardPages/baselineParameters.py', title = 'Baseline Parameters', 
-    icon = ':material/variable_insert:'
+    "DashboardPages/baselineParameters.py",
+    title="Baseline Parameters",
+    icon=":material/variable_insert:",
 )
 scenarioParameters = st.Page(
-    'DashboardPages/scenarioParameters.py', title = 'Scenario Parameters', 
-    icon = ':material/variable_add:'
+    "DashboardPages/scenarioParameters.py",
+    title="Scenario Parameters",
+    icon=":material/variable_add:",
 )
 infectionGraphs = st.Page(
-    'DashboardPages/chartDemonstration.py', 
-    title = 'Infection Over Time Graphs', icon = ':material/chart_data:'
+    "DashboardPages/chartDemonstration.py",
+    title="Infection Over Time Graphs",
+    icon=":material/chart_data:",
 )
 healthTables = st.Page(
-    'DashboardPages/tableCreation.py', title = 'Health Burden Tables', 
-    icon = ':material/table_chart_view:'
+    "DashboardPages/tableCreation.py",
+    title="Health Burden Tables",
+    icon=":material/table_chart_view:",
 )
 
 pages = {
-    'SMRG Flusim Web Dashboard': [modelDescription],
-    'Parameter Configuration': [baselineParameters, scenarioParameters],
-    'Results Visualisation': [infectionGraphs, healthTables]
+    "SMRG Flusim Web Dashboard": [modelDescription],
+    "Parameter Configuration": [baselineParameters, scenarioParameters],
+    "Results Visualisation": [infectionGraphs, healthTables],
 }
 
 # Initialise session variables used globally by the dashboard
-# Use current time (Unix) as session ID so that different simulations 
+# Use current time (Unix) as session ID so that different simulations
 # aren't mixed up by the server
 sessionParameters = {
-    'simulationInProgress': False, 'scenarioCount': 0,
-    'sessionID': int(datetime.now().timestamp()),
-    'scenarioSetParamsExtra': {1: [], 2: [], 3: [], 4: [], 5: []}, 
-    'scenarioSetParams': {1: [], 2: [], 3: [], 4: [], 5: []}
+    "simulationInProgress": False,
+    "scenarioCount": 0,
+    "sessionID": int(datetime.now().timestamp()),
+    "scenarioSetParamsExtra": {1: [], 2: [], 3: [], 4: [], 5: []},
+    "scenarioSetParams": {1: [], 2: [], 3: [], 4: [], 5: []},
 }
-for parameter, default in sessionParameters.items(): 
+for parameter, default in sessionParameters.items():
     session[parameter] = session.get(parameter, default)
 
 # Display toasts
-stn.notify(remove = True)
+stn.notify(remove=True)
 
 # Initialise and run the application pages
 flusimPages = st.navigation(pages)
@@ -101,86 +107,104 @@ flusimPages.run()
 # TODO: consider adding progress updates to the sidebar (time remaining,
 # progress bars, server availability etc.)
 runModelButton = st.sidebar.button(
-    label = (
-        'Running simulation...' 
-        if session.simulationInProgress 
-        else 'Run Simulation'
-    ), 
-    on_click = runSimulationButton, key = '_runSim',
-    disabled = session.simulationInProgress, type = 'primary', 
-    icon = (
-        ':material/hourglass:' 
-        if session.simulationInProgress 
-        else ':material/motion_play:'
-    ), 
-    help = '''
-        Send a request to the *Flusim* model server to run the model 
-        with the specified parameters. Once the request has been made, 
-        you will be unable to run the model again until it completes, 
-        so make sure you have configured your parameters to appropriate 
+    label=(
+        "Running simulation..." if session.simulationInProgress else "Run Simulation"
+    ),
+    on_click=runSimulationButton,
+    key="_runSim",
+    disabled=session.simulationInProgress,
+    type="primary",
+    icon=(
+        ":material/hourglass:"
+        if session.simulationInProgress
+        else ":material/motion_play:"
+    ),
+    help=(
+        """
+        Send a request to the *Flusim* model server to run the model
+        with the specified parameters. Once the request has been made,
+        you will be unable to run the model again until it completes,
+        so make sure you have configured your parameters to appropriate
         values before clicking.
-    ''' if not session.simulationInProgress else '''
-        A simulation is already running; please wait for it to conclude 
+    """
+        if not session.simulationInProgress
+        else """
+        A simulation is already running; please wait for it to conclude
         before running another one.
-    '''
+    """
+    ),
 )
 
 
 # Fragment to regularly check if model results have been received yet
-@st.fragment(run_every = 1)
+@st.fragment(run_every=1)
 def updateData():
     if session.simulationInProgress and not resultQueue.empty():
         processedData = resultQueue.get()
-        appLog.info(
-            f'[updateData] Processing the following data:\n{processedData}'
-        )
+        appLog.info(f"[updateData] Processing the following data:\n{processedData}")
 
         # Check if the server returned an error instead of proper data
         if isinstance(processedData, list):
             successes = 0
-            scenarios = 4 if usePresetData or usePresetParams else session.scenarioCount + 1
-            for data, tag in processedData: 
+            scenarios = (
+                4 if usePresetData or usePresetParams else session.scenarioCount + 1
+            )
+            for data, tag in processedData:
                 # Further error checking
-                if len(data) == 0: stn.toast(
-                    f'''
-                    :red-badge[Error]: No data was present on one 
-                    or more of the files received from the server. 
-                    Please make sure your parameters do not possess any 
+                if len(data) == 0:
+                    stn.toast(
+                        """
+                    :red-badge[Error]: No data was present on one
+                    or more of the files received from the server.
+                    Please make sure your parameters do not possess any
                     errors and try again.
-                ''', icon = ':material/tab_unselected:')
-                elif tag in {'EpidemicCumulative', 'EpidemicDaily'} and len(
-                    data['Scenario'].value_counts()
-                ) != scenarios: 
-                    stn.toast(f'''
-                        :red-badge[Error]: One or more scenarios 
-                        were not run correctly by the simulation 
-                        server. Please ensure all scenarios do not 
+                """,
+                        icon=":material/tab_unselected:",
+                    )
+                elif (
+                    tag in {"EpidemicCumulative", "EpidemicDaily"}
+                    and len(data["Scenario"].value_counts()) != scenarios
+                ):
+                    stn.toast(
+                        """
+                        :red-badge[Error]: One or more scenarios
+                        were not run correctly by the simulation
+                        server. Please ensure all scenarios do not
                         possess any errors and try again.
-                    ''', icon = ':material/donut_small:')
-                else: 
+                    """,
+                        icon=":material/donut_small:",
+                    )
+                else:
                     successes += 1
-                    session[f'modelData{tag}'] = data
-            
+                    session[f"modelData{tag}"] = data
+
             # Update parameters
             session.simulationEndTime = datetime.now()
             totalTime = session.simulationEndTime - session.simulationStartTime
-            timeString = f'{totalTime.seconds // 60}:{totalTime.seconds % 60}'
-            if successes == 3: stn.toast(
-                f'Simulation complete! Total duration: {timeString}', 
-                icon = ":material/check_circle:"
-            )
-            elif successes > 0: stn.toast(f'''
-                Simulation complete,  
-                though some analyses had errors. 
+            timeString = f"{totalTime.seconds // 60}:{totalTime.seconds % 60}"
+            if successes == 3:
+                stn.toast(
+                    f"Simulation complete! Total duration: {timeString}",
+                    icon=":material/check_circle:",
+                )
+            elif successes > 0:
+                stn.toast(
+                    f"""
+                Simulation complete,
+                though some analyses had errors.
                 Total duration: {timeString}
-            ''', icon = ":material/flaky:")
-            appLog.info(f'''
-                [updateData] Data processing is complete, 
+            """,
+                    icon=":material/flaky:",
+                )
+            appLog.info(
+                f"""
+                [updateData] Data processing is complete,
                 with {scenarios - successes + 1} errors.
-            ''')
+            """
+            )
         else:
             appLog.error(
-                '[updateData] Received data was atypical. Contents: ' 
+                "[updateData] Received data was atypical. Contents: "
                 + str(processedData)
             )
 
@@ -188,67 +212,103 @@ def updateData():
             if isinstance(processedData, tuple):
                 # Errors with exceptions attached
                 errorType, e = processedData
-                if errorType == 'ClientConnectorError': stn.toast(f'''
-                    :red-badge[Error]: Could not connect to the 
-                    simulation server. Please make sure you are 
-                    connected to the same network as the server, then 
-                    try again.
-                ''', icon = ':material/link_off:')
-                elif errorType == 'ClientResponseError500': stn.toast(f'''
-                    :red-badge[Error]: Simulation server had an 
-                    internal error. Please try again later.
-                ''', icon = ':material/error:')
-                elif errorType == 'ValueError': stn.toast(f'''
-                    :red-badge[Error]: The data received from the 
-                    simulation server was incorrectly formatted. Please 
-                    make sure your parameters do not possess any errors 
-                    and try again.
-                ''', icon = ':material/broken_image:')  
-                elif errorType == 'InvalidSchemaError':
-                    stn.toast(f'''
-                        :red-badge[Error]: The parameters sent to the 
-                        server do not match the required format. Please 
-                        check your parameters for errors and try again.
-                    ''', icon = ':material/schema:')
+                if errorType == "ClientConnectorError":
                     stn.toast(
-                        f':red-badge[Response Body]: {e.response}', 
-                        icon = ':material/breaking_news:'
+                        """
+                    :red-badge[Error]: Could not connect to the
+                    simulation server. Please make sure you are
+                    connected to the same network as the server, then
+                    try again.
+                """,
+                        icon=":material/link_off:",
                     )
-                else: stn.toast(f'''
-                    :red-badge[Error]: The simulation server 
+                elif errorType == "ClientResponseError500":
+                    stn.toast(
+                        """
+                    :red-badge[Error]: Simulation server had an
+                    internal error. Please try again later.
+                """,
+                        icon=":material/error:",
+                    )
+                elif errorType == "ValueError":
+                    stn.toast(
+                        """
+                    :red-badge[Error]: The data received from the
+                    simulation server was incorrectly formatted. Please
+                    make sure your parameters do not possess any errors
+                    and try again.
+                """,
+                        icon=":material/broken_image:",
+                    )
+                elif errorType == "InvalidSchemaError":
+                    stn.toast(
+                        """
+                        :red-badge[Error]: The parameters sent to the
+                        server do not match the required format. Please
+                        check your parameters for errors and try again.
+                    """,
+                        icon=":material/schema:",
+                    )
+                    stn.toast(
+                        f":red-badge[Response Body]: {e.response}",
+                        icon=":material/breaking_news:",
+                    )
+                else:
+                    stn.toast(
+                        """
+                    :red-badge[Error]: The simulation server
                     encountered an error. Please try again later.
-                ''', icon = ':material/error:')
+                """,
+                        icon=":material/error:",
+                    )
                 stn.toast(
-                    f':red-badge[Full Error Message]: {e}', 
-                    icon = ':material/breaking_news:'
+                    f":red-badge[Full Error Message]: {e}",
+                    icon=":material/breaking_news:",
                 )
-            
+
             # Errors without exception messages to send
-            elif isinstance(processedData, pd.DataFrame): stn.toast(f'''
-                :red-badge[Error]: The data was not processed 
+            elif isinstance(processedData, pd.DataFrame):
+                stn.toast(
+                    """
+                :red-badge[Error]: The data was not processed
                 correctly. Please try again later.
-            ''', icon = ':material/data_alert:')
-            elif processedData == 'EmptyZipFile': stn.toast(f'''
-                :red-badge[Error]: The simulation server did not 
-                return any readable files. Please make sure your 
+            """,
+                    icon=":material/data_alert:",
+                )
+            elif processedData == "EmptyZipFile":
+                stn.toast(
+                    """
+                :red-badge[Error]: The simulation server did not
+                return any readable files. Please make sure your
                 parameters do not possess any errors and try again.
-            ''', icon = ':material/unknown_document:')
-            else: stn.toast(f'''
-                :red-badge[Error]: An unknown error occurred. Please 
+            """,
+                    icon=":material/unknown_document:",
+                )
+            else:
+                stn.toast(
+                    """
+                :red-badge[Error]: An unknown error occurred. Please
                 try again later.
-            ''', icon = ':material/error:')
-                
+            """,
+                    icon=":material/error:",
+                )
+
         # Make pending data no longer pending
         pendingData = {
-            'Community', 'ScenarioNames', 'ScenarioCount', 
-            'HealthOutcomeRates', 'MortalityRates'
+            "Community",
+            "ScenarioNames",
+            "ScenarioCount",
+            "HealthOutcomeRates",
+            "MortalityRates",
         }
-        for name in pendingData: 
-            session[f'Data{name}'] = session.get(f'PendingData{name}')
+        for name in pendingData:
+            session[f"Data{name}"] = session.get(f"PendingData{name}")
         session.scenariosToUse = session.DataScenarioNames
 
         # Re-enable running new simulations and using their data
         session.ChartGenerated = False
         session.simulationInProgress = False
         st.rerun()
+
+
 updateData()
